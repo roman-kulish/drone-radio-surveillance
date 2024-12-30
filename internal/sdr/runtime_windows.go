7350cf6012eb321c6b38dc85c7e26454616d7685
Roman Kulish <roman.kulish@gmail.com>
2024-12-30 11:11:06 +1100

    Look inside working directory only

--- internal/sdr/runtime_windows.go
+++ internal/sdr/runtime_windows.go
@@ -9,35 +9,20 @@ import (
 )
 
 func FindRuntime(runtime string) (string, error) {
-	lookup := []string{}
-
-	exePath, err := os.Executable()
-	if err != nil {
-		return "", fmt.Errorf("failed to get executable path: %w", err)
-	}
-
-	lookup = append(lookup, filepath.Dir(exePath))
-
-	exePath, err = os.Getwd()
+	wd, err := os.Getwd()
 	if err != nil {
 		return "", fmt.Errorf("failed to get current working directory: %w", err)
 	}
 
-	lookup = append(lookup, exePath)
-
-	for _, exeDir := range lookup {
-		matches, err := filepath.Glob(filepath.Join(exeDir, "bin", "*", "windows", "x64", fmt.Sprintf("%s.exe", runtime)))
-		if err != nil || len(matches) == 0 {
-			continue // continue to next directory
-		}
-
-		binPath := matches[0]
-		if _, err = os.Stat(binPath); err != nil {
-			continue // continue to next directory
-		}
-
-		return binPath, nil
+	matches, err := filepath.Glob(filepath.Join(wd, "bin", "*", "windows", "x64", fmt.Sprintf("%s.exe", runtime)))
+	if err != nil || len(matches) == 0 {
+		return "", fmt.Errorf("failed to find binary '%s'", runtime)
 	}
 
-	return "", fmt.Errorf("failed to find binary '%s'", runtime)
+	binPath := matches[0]
+	if _, err = os.Stat(binPath); err != nil {
+		return "", fmt.Errorf("failed to stat binary '%s': %w", binPath, err)
+	}
+
+	return binPath, nil
 }
