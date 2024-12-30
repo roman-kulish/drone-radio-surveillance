0c1283931a8b647814cd16c1c99188114d1d2f63
Roman Kulish <roman.kulish@gmail.com>
2024-12-30 13:24:53 +1100

    Implemented sweeper configuration and devices creation

--- cmd/sweeper/app/config.go
+++ cmd/sweeper/app/config.go
@@ -1,6 +1,11 @@
 package app
 
 import (
+	"fmt"
+	"log/slog"
+	"os"
+
+	"gopkg.in/yaml.v3"
 	"gtihub.con/roman-kulish/radio-surveillance/internal/sdr/hackrf"
 	"gtihub.con/roman-kulish/radio-surveillance/internal/sdr/rtl"
 )
@@ -13,31 +18,92 @@ const (
 	TelemetryTypeMagnetometer TelemetryType = "magnetometer"
 )
 
-type TelemetryType string
-
-type DeviceTypeConfig interface {
-	*rtl.Config | *hackrf.Config
+// yamlNode is a custom type for unmarshalling raw YAML nodes
+type yamlNode struct {
+	*yaml.Node
 }
 
+// UnmarshalYAML implements the yaml.Unmarshaler interface for custom deserialization of yamlNode from YAML input.
+func (y *yamlNode) UnmarshalYAML(value *yaml.Node) error {
+	y.Node = value
+	return nil
+}
+
+type TelemetryType string
+
 // Config represents the main application configuration
-type Config[T DeviceTypeConfig] struct {
-	Settings  Settings          `yaml:"settings"`
-	Devices   []DeviceConfig[T] `yaml:"devices"`
-	Telemetry TelemetryConfig   `yaml:"telemetry"`
-	Storage   StorageConfig     `yaml:"storage"`
+type Config struct {
+	Settings  Settings        `yaml:"settings"`
+	Devices   []DeviceConfig  `yaml:"devices"`
+	Telemetry TelemetryConfig `yaml:"telemetry"`
+	Storage   StorageConfig   `yaml:"storage"`
 }
 
 // Settings represents global application settings
 type Settings struct {
-	LogLevel string `yaml:"logLevel"`
+	LogLevel slog.Level `yaml:"logLevel"`
+}
+
+func (s *Settings) UnmarshalYAML(value *yaml.Node) error {
+	var t struct {
+		LogLevel string `yaml:"logLevel"`
+	}
+	if err := value.Decode(&t); err != nil {
+		return err
+	}
+
+	s.LogLevel = slog.LevelInfo
+	return s.LogLevel.UnmarshalText([]byte(t.LogLevel))
 }
 
 // DeviceConfig represents a single device configuration
-type DeviceConfig[T DeviceTypeConfig] struct {
+type DeviceConfig struct {
 	Name    string `yaml:"name"`
 	Type    string `yaml:"type"`
 	Enabled bool   `yaml:"enabled"`
-	Config  T      `yaml:"configFile"`
+	Config  any    `yaml:"config"`
+}
+
+// UnmarshalYAML implements the yaml.Unmarshaler interface for custom deserialization of DeviceConfig from YAML input.
+func (d *DeviceConfig) UnmarshalYAML(value *yaml.Node) error {
+	var t struct {
+		Name    string   `yaml:"name"`
+		Type    string   `yaml:"type"`
+		Enabled bool     `yaml:"enabled"`
+		Config  yamlNode `yaml:"config"`
+	}
+	if err := value.Decode(&t); err != nil {
+		return err
+	}
+
+	dc := DeviceConfig{
+		Name:    t.Name,
+		Type:    t.Type,
+		Enabled: t.Enabled,
+	}
+	switch t.Type {
+	case "rtl-sdr":
+		var c rtl.Config
+		if err := t.Config.Decode(&c); err != nil {
+			return err
+		}
+
+		dc.Config = &c
+
+	case "hackrf":
+		var c hackrf.Config
+		if err := t.Config.Decode(&c); err != nil {
+			return err
+		}
+
+		dc.Config = &c
+
+	default:
+		return fmt.Errorf("unknown device type: %s", t.Type)
+	}
+
+	*d = dc
+	return nil
 }
 
 // TelemetryConfig represents telemetry settings
@@ -54,4 +120,19 @@ type TelemetryConfig struct {
 type StorageConfig struct {
 	DataDirectory string `yaml:"dataDirectory"`
 	MaxBatchSize  int    `yaml:"maxBatchSize"`
+}
+
+// LoadConfig reads a configuration file from the specified path and parses it into a Config struct.
+func LoadConfig(path string) (*Config, error) {
+	configFile, err := os.ReadFile(path)
+	if err != nil {
+		return nil, fmt.Errorf("reading configuration file: %w", err)
+	}
+
+	var config Config
+	if err = yaml.Unmarshal(configFile, &config); err != nil {
+		return nil, fmt.Errorf("parsing configuration file: %w", err)
+	}
+
+	return &config, nil
 }
--- cmd/sweeper/main.go
+++ cmd/sweeper/main.go
@@ -1,5 +1,65 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"log/slog"
+	"os"
+
+	"gtihub.con/roman-kulish/radio-surveillance/cmd/sweeper/app"
+	"gtihub.con/roman-kulish/radio-surveillance/internal/sdr"
+	"gtihub.con/roman-kulish/radio-surveillance/internal/sdr/hackrf"
+	"gtihub.con/roman-kulish/radio-surveillance/internal/sdr/rtl"
+)
+
 func main() {
-	println("Hello, World!")
+	var logLevel slog.LevelVar
+	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: &logLevel}))
+
+	var configPath string
+	flag.StringVar(&configPath, "c", "", "Path to the configuration file")
+	flag.Parse()
+
+	if configPath == "" {
+		logger.Error("no configuration file provided")
+		os.Exit(1)
+	}
+
+	config, err := app.LoadConfig(configPath)
+	if err != nil {
+		logger.Error(fmt.Sprintf("failed to load configuration file: %s", err.Error()), slog.String("path", configPath))
+		os.Exit(1)
+	}
+
+	logLevel.Set(config.Settings.LogLevel)
+
+	var devices []*sdr.Device
+	for _, deviceConfig := range config.Devices {
+		if !deviceConfig.Enabled {
+			continue
+		}
+		switch deviceConfig.Type {
+		case "rtl-sdr":
+			runner, err := rtl.New(deviceConfig.Config.(*rtl.Config))
+			if err != nil {
+				logger.Error(fmt.Sprintf("failed to create RTL-SDR device: %s", err.Error()))
+				os.Exit(1)
+			}
+			devices = append(devices, sdr.NewDevice(deviceConfig.Name, runner, sdr.WithLogger(logger)))
+
+		case "hackrf":
+			runner, err := hackrf.New(deviceConfig.Config.(*hackrf.Config))
+			if err != nil {
+				logger.Error(fmt.Sprintf("failed to create HackRF device: %s", err.Error()))
+				os.Exit(1)
+			}
+			devices = append(devices, sdr.NewDevice(deviceConfig.Name, runner, sdr.WithLogger(logger)))
+
+		default:
+			logger.Error(fmt.Sprintf("failed to create device: unknown type '%s'", deviceConfig.Type))
+			os.Exit(1)
+		}
+	}
+
+	// TODO: collector
 }
